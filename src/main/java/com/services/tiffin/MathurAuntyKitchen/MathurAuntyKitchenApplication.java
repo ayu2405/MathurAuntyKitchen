497862fba49815d12f4ef2a6ed37a0f89055c3b4
Ayushi <ashi.211822@gmail.com>
2022-11-19 18:15:47 +0530

    Initial spring starter files

---
+++ src/main/java/com/services/tiffin/MathurAuntyKitchen/MathurAuntyKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.services.tiffin.MathurAuntyKitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathurAuntyKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathurAuntyKitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/services/tiffin/MathurAuntyKitchen/MathurAuntyKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.tiffin.MathurAuntyKitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathurAuntyKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
