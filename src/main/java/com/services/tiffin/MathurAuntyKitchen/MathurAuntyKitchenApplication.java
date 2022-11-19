4192ffba3ac3ac9156c5caf4f8cdfa2d40ee0fab
Ayushi <ashi.211822@gmail.com>
2022-11-19 22:01:49 +0530

    Application and UI created by jhipster

--- src/main/java/com/services/tiffin/MathurAuntyKitchen/MathurAuntyKitchenApplication.java
+++ src/main/java/com/services/tiffin/MathurAuntyKitchen/MathurAuntyKitchenApplication.java
@@ -3,11 +3,10 @@ package com.services.tiffin.MathurAuntyKitchen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class MathurAuntyKitchenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MathurAuntyKitchenApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MathurAuntyKitchenApplication.class, args);
+    }
 }
---
+++ src/test/java/com/services/tiffin/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.services.tiffin;
+
+import com.services.tiffin.MathurAuntyKitchenApp;
+import com.services.tiffin.config.AsyncSyncConfiguration;
+import com.services.tiffin.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { MathurAuntyKitchenApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
